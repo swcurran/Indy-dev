9970b471c254ce43d8bc2e6593528ee539adbb1e
Stephen Curran <swcurran@gmail.com>
2018-10-17 20:04:15 +0000

    Fix bug in start_node; update to indy-node 1.6
    Signed-off-by: Stephen Curran <swcurran@gmail.com>

--- scripts/start_node.sh
+++ scripts/start_node.sh
@@ -9,15 +9,16 @@ NODE_PORT=$((START_PORT + ( NODE_NUM * 2 ) - 1 ))
 
 if [ ! -d "/var/lib/indy/sandbox/keys" ]; then
     echo "Ledger does not exist - Creating..."
-if [ ! -z "$IPS" ]; then
-    echo von_generate_transactions -s "$IPS" -n "$NODE_NUM"
-    von_generate_transactions -s "$IPS" -n "$NODE_NUM"
-elif [ ! -z "$IP" ]; then
-    echo von_generate_transactions -i "$IP" -n "$NODE_NUM"
-    von_generate_transactions -i "$IP" -n "$NODE_NUM"
-else
-    echo von_generate_transactions -n "$NODE_NUM"
-    von_generate_transactions -n "$NODE_NUM"
+    if [ ! -z "$IPS" ]; then
+        echo von_generate_transactions -s "$IPS" -n "$NODE_NUM"
+        von_generate_transactions -s "$IPS" -n "$NODE_NUM"
+    elif [ ! -z "$IP" ]; then
+        echo von_generate_transactions -i "$IP" -n "$NODE_NUM"
+        von_generate_transactions -i "$IP" -n "$NODE_NUM"
+    else
+        echo von_generate_transactions -n "$NODE_NUM"
+        von_generate_transactions -n "$NODE_NUM"
+    fi
 fi
 
 echo start_indy_node "Node""$NODE_NUM" $HOST $NODE_PORT $HOST $(( NODE_PORT + 1 ))
